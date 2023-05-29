d48f9ce17a23d6fb6750060687ad1b7057de4f90
Mike Rouleau <44850854+mikerouleau@users.noreply.github.com>
2023-05-29 19:21:22 -0400

    implement packed_Struct, anyhow, write_reg

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod tmc2209;
+use packed_struct::prelude::*;
 
 fn main() {
     let mut tmc = tmc2209::TMC2209::new("/dev/serial0", 0x0, 115_200u32);
-    let reg = 0x6F;
+    tmc.test_uart().expect("msg");
+    let reg = 0x00;
     let received = tmc.read_reg(reg).expect("Read Failed");
     for i in received {
         println!("Received byte: {:08b}", i);
     }
+    let unpacked =
+        tmc2209::reg::IOIN::unpack_from_slice(&received).expect("failed to unpack response");
+
+    println!("VERSION: {:}", unpacked.version);
+
+    tmc.set_direction(false).expect("errorrrorrr")
 }
--- src/tmc2209.rs
+++ src/tmc2209.rs
@@ -1,12 +1,14 @@
+use anyhow::{anyhow, Result};
+use packed_struct::prelude::*;
 use rppal::uart::{Parity, Uart};
 use std::convert::TryInto;
 use std::path::PathBuf;
 use std::time::Instant;
 
 mod err;
-mod reg;
+pub mod reg;
+use crate::tmc2209::err::TMC2209Error;
 
-#[allow(dead_code)]
 pub struct TMC2209 {
     pub path: PathBuf,
     pub addr: u8,
@@ -15,21 +17,25 @@ pub struct TMC2209 {
     sync_byte: u8,
 }
 
+#[allow(dead_code)]
 impl TMC2209 {
     pub fn new(path: &str, addr: u8, baud_rate: u32) -> Self {
+        let mut uart = Uart::with_path(path, baud_rate, Parity::None, 8, 1).unwrap();
+        uart.set_write_mode(true).expect("failed to set blocking");
         TMC2209 {
             path: path.into(),
             addr,
             baud_rate,
-            uart: Uart::with_path(path, baud_rate, Parity::None, 8, 1).unwrap(),
+            uart,
             sync_byte: 0x05,
         }
     }
 
-    pub fn read_reg(&mut self, reg: u8) -> Result<[u8; 4], err::TMC2209Error> {
+    pub fn read_reg(&mut self, reg: u8) -> Result<[u8; 4]> {
         let crc = crc8_atm(&[self.sync_byte, self.addr, reg]);
         let frame = [self.sync_byte, self.addr, reg, crc];
 
+        self.uart.flush(rppal::uart::Queue::Both)?;
         self.uart.write(&frame).expect("Frame not written.");
 
         let mut buffer = [0u8; 1];
@@ -49,13 +55,84 @@ impl TMC2209 {
                     let data: [u8; 4] = response[3..7].try_into().unwrap();
                     return Ok(data);
                 } else {
-                    return Err(err::TMC2209Error::CRCMismatch);
+                    return Err(anyhow!(err::TMC2209Error::CRCMismatch));
                 }
 
                 //arbitrarily long timeout, seems plently long in testing
             } else if start.elapsed().as_millis() > 15 {
-                return Err(err::TMC2209Error::TimedOut);
+                return Err(anyhow!(err::TMC2209Error::TimedOut));
             }
+        }
+    }
+
+    pub fn write_reg(&mut self, reg: u8, data: [u8; 4]) -> anyhow::Result<()> {
+        let crc = crc8_atm(&[
+            self.sync_byte,
+            self.addr,
+            reg,
+            data[0],
+            data[1],
+            data[2],
+            data[3],
+        ]);
+        let frame = [
+            self.sync_byte,
+            self.addr,
+            reg,
+            data[0],
+            data[1],
+            data[2],
+            data[3],
+            crc,
+        ];
+
+        self.uart.flush(rppal::uart::Queue::Both)?;
+        self.uart.write(&frame)?;
+        self.uart.drain()?;
+        Ok(())
+    }
+
+    pub fn test_uart(&mut self) -> Result<()> {
+        let response = self.read_reg(reg::IOIN::ADDR)?;
+        let unpacked = reg::IOIN::unpack_from_slice(&response);
+
+        if unpacked.is_ok() {
+            let data = unpacked.unwrap();
+            (data.zero1 == 0.into())
+                .then_some(())
+                .ok_or(TMC2209Error::UnexpectedResponse)?;
+            (data.zero2 == 0.into())
+                .then_some(())
+                .ok_or(TMC2209Error::UnexpectedResponse)?;
+            (data.pdn_uart == true)
+                .then_some(())
+                .ok_or(TMC2209Error::UnexpectedResponse)?;
+
+            return Ok(());
+        } else {
+            return Err(anyhow!(TMC2209Error::Unknown));
+        }
+    }
+
+    pub fn set_direction(&mut self, dir: bool) -> Result<(), anyhow::Error> {
+        let mut packed = self.read_reg(reg::GCONF::ADDR)?;
+        let mut unpacked = reg::GCONF::unpack_from_slice(&packed)?;
+        unpacked.shaft = dir;
+        packed = unpacked.pack()?;
+
+        let prior_cnt = reg::IFCNT::unpack_from_slice(&self.read_reg(reg::IFCNT::ADDR)?)?.cnt;
+
+        self.write_reg(reg::GCONF::ADDR + reg::WRITE_OFFSET, packed)?;
+
+        let response = self.read_reg(reg::IFCNT::ADDR)?;
+        let after_cnt = reg::IFCNT::unpack_from_slice(&response)?.cnt;
+
+        if prior_cnt < after_cnt {
+            println!("SUCCESS");
+            return Ok(());
+        } else {
+            println!("FAIL");
+            return Err(anyhow!(err::TMC2209Error::Unknown));
         }
     }
 }
