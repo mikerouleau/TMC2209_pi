65682fd84debf50fae89b6e89683f8365a5e9ede
Mike Rouleau <44850854+mikerouleau@users.noreply.github.com>
2023-05-29 19:21:21 -0400

    implement crc8 function

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ fn main() {
 
     let baud_rate = 115_200u32;
     let mut uart = Uart::with_path("/dev/serial0", baud_rate, Parity::None, 8, 1).unwrap();
+    let sync_byte = 0x5;
+    let addr = 0x0;
     let reg = 0x6;
-    let crc = 0xe8;
-    let frame = [0x55, 0x0, reg, crc];
+    let crc = crc8_atm(&[sync_byte, addr, reg], 0);
+    let frame = [sync_byte, addr, reg, crc];
 
     let mut buffer = [0u8; 1];
 
@@ -33,3 +35,20 @@ fn main() {
 //     addr: u32,
 //     port: Port,
 // }
+
+fn crc8_atm(datagram: &[u8], initial_value: u8) -> u8 {
+    let mut crc: u8 = initial_value;
+
+    for bytey in datagram {
+        let mut byte = *bytey;
+        for _ in 0..8 {
+            if (crc >> 7) ^ (byte & 0x01) != 0 {
+                crc = ((crc << 1) ^ 0x07) & 0xFF;
+            } else {
+                crc = (crc << 1) & 0xFF;
+            }
+            byte = byte >> 1;
+        }
+    }
+    crc
+}
