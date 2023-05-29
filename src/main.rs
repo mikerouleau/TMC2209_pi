d47da0e0302b9ab5f43ea2843d6295e1330cec45
Mike Rouleau <44850854+mikerouleau@users.noreply.github.com>
2023-05-29 19:21:22 -0400

    cleanup, use set_rpm

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
 mod tmc2209;
-use packed_struct::prelude::*;
-use std::thread;
-use std::time::Duration;
+// use packed_struct::prelude::*;
+// use std::thread;
+// use std::time::Duration;
 
 fn main() {
     let mut tmc = tmc2209::TMC2209::new("/dev/serial0", 0x0, 115_200u32, 16, 20, 21, 26).unwrap();
-    tmc.test_uart().expect("msg");
-    // let reg = 0x06F;
-    // let received = tmc.read_reg(reg).expect("Read Failed");
-    // for i in received {
-    //     println!("Received byte: {:08b}", i);
-    // }
-    // let unpacked =
-    //     tmc2209::reg::IOIN::unpack_from_slice(&received).expect("failed to unpack response");
 
-    // println!("VERSION: {:}", unpacked.version);
-
-    // tmc.set_shaft(false).expect("errorrrorrr");
     tmc.reset_position();
+
     println!("position: {:}", tmc.position);
 
-    tmc.enable_output(true).unwrap();
-    tmc.go_to_position(-100).unwrap();
-    tmc.go_to_position(200).unwrap();
-    tmc.go_to_position(-200).unwrap();
+    let chopchop = tmc.get_chopconf().unwrap();
+    println!("mres: {:}", chopchop.mres);
+
+    tmc.set_rpm(10).unwrap();
+    tmc.go_to_position(400).unwrap();
+    tmc.set_rpm(50).unwrap();
     tmc.go_to_position(0).unwrap();
-
-    tmc.set_vel(10).unwrap();
-
-    println!("position: {:}", tmc.position);
-    thread::sleep(Duration::from_secs(1));
-    tmc.set_vel(0).unwrap();
 }
